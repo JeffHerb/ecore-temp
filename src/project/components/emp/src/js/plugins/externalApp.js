a8f109820c6e11e15ce792e0ec5aabb2986eaef4
Jeff Herb <herb.jeff@gmail.com>
2020-04-27 20:20:49 -0400

    Fixed account and support menus to work with the menu changes

--- src/project/components/emp/src/js/plugins/externalApp.js
+++ src/project/components/emp/src/js/plugins/externalApp.js
@@ -1,3 +1,4 @@
+/*jshint loopfunc: true */
 define(['render'], function (render) {
 
     var _priv = {};
@@ -48,7 +49,6 @@ define(['render'], function (render) {
             // Generate the contents
             render.section(undefined, userAcctJSON.popover, 'return', function(html) {
 
-                console.log(html);
                 _priv.$userPopover = $.popover($userAccountButton, {
                     html: html,
                     display: {
@@ -64,10 +64,63 @@ define(['render'], function (render) {
             if (!_priv.$userPopover) {
                 generatePopver();
 
+                _priv.setUpExpandables(_priv.$userPopover.$popover[0]);
+
                 _priv.$userPopover.show();
             }
 
         });
+
+    };
+
+    _priv.expandToggle = function(dExpandControl) {
+
+        var dCurrentNode = dExpandControl;
+
+        while(true) {
+
+            if (dCurrentNode.nodeName === "BODY") {
+                dCurrentNode = false;
+                break;
+            }
+
+            if (dCurrentNode.nodeType === 1) {
+
+                if (dCurrentNode.classList.contains('emp-expandable-control')) {
+                    break;
+                }
+
+            }
+
+            dCurrentNode = dCurrentNode.parentNode;
+        }
+
+        if (dCurrentNode) {
+
+            dSpan = dCurrentNode;
+    
+            if (dSpan.classList.contains('emp-collapse-children')) {
+                dSpan.classList.remove('emp-collapse-children');
+            }
+            else {
+                dSpan.classList.add('emp-collapse-children');
+            }
+        }
+    };
+
+    _priv.setUpExpandables = function(dContents) {
+
+        var dnExpandables = dContents.querySelectorAll('.emp-expandable-control');
+
+        if (dnExpandables && dnExpandables.length) {
+
+            for (var e = 0, eLen = dnExpandables.length; e < eLen; e++) {
+
+                dnExpandables[e].addEventListener('click', function(evt, dElem) {
+                    _priv.expandToggle(evt.target);
+                });
+            }
+        }
 
     };
 
@@ -132,22 +185,64 @@ define(['render'], function (render) {
             var dUserAccountButton = document.querySelector('#userAcct');
             var dSupportButton = document.querySelector('#agencyHelp');
 
-            // var userAccountJSON = false;
-            // var supportJSON = false;
+            console.log(fwData.menus);
 
-            // Save off the user account information
-            // if (fwData.menus.userAcct) {
-            //     userAccountJSON = fwData.menus.userAcct;
-            // }
+            if (dUserAccountButton && fwData.menus.userAccount) {
 
-            // // Save off the support button information
-            // if (fwData.menus.support) {
-            //     supportJSON = fwData.menus.support;
-            // }
+                var userAccountPopover = {
+                    "popover": {
+                        "contents": [                                
+                            {
+                                "template": "popover",
+                                "contents": [
+                                ]
+                            }
+                        ]
+                    }
+                };
 
-            if (dUserAccountButton && fwData.menus.userAcct) {
+                // Construct the user info section if it exists
+                if (fwData && fwData.context && fwData.context.screen && fwData.context.screen.userInfo) {
+                    
+                    var oUserInfo = {
+                        "type": "external-auth-user",
+                        "template": "composite",
+                        "properties": {
+                            "realName": fwData.context.screen.userInfo.realName,
+                            "companyName": fwData.context.screen.userInfo.companyName,
+                            "userid": fwData.context.screen.userInfo.userid
+                        }
+                    };
 
-                dUserAccountButton.addEventListener('click', _events.userAccountPopover.bind(null, fwData.menus.userAcct));
+                    userAccountPopover.popover.contents[0].contents.push(oUserInfo);
+                }
+
+                // Construct the account info section if it exists
+                if (fwData && fwData.context && fwData.context.screen && fwData.context.screen.accountInfo) {
+                    
+                    var oUserAcctInfo = {
+                        "type": "external-account-demo",
+                        "template": "composite",
+                        "properties": {
+                            "account": fwData.context.screen.accountInfo.account,
+                            "role": fwData.context.screen.accountInfo.role
+                        }
+                    };
+
+                    userAccountPopover.popover.contents[0].contents.push(oUserAcctInfo);
+                }
+
+                // Add the account menu if it exists
+                if (fwData && fwData.menus && fwData.menus.userAccount) {
+                    userAccountPopover.popover.contents[0].contents.push(fwData.menus.userAccount);
+                }
+
+                // Add the system menu if it exists
+                if (fwData && fwData.menus && fwData.menus.system) {
+                    userAccountPopover.popover.contents[0].contents.push(fwData.menus.system);
+                }
+
+                dUserAccountButton.addEventListener('click', _events.userAccountPopover.bind(null, userAccountPopover));
 
                 journal.log({ type: 'info', owner: 'UI', module: 'emp', function: 'externalApp' }, 'User Account Menu setup');
             }
@@ -155,9 +250,63 @@ define(['render'], function (render) {
                 journal.log({ type: 'error', owner: 'UI', module: 'emp', function: 'externalApp' }, 'User Account Menu not setup because something is missing');
             }
 
-            if (dSupportButton && fwData.menus.support) {
+            if (dSupportButton && fwData.menus.supportContact) {
 
-                dSupportButton.addEventListener('click', _events.getSupportPopover.bind(null, fwData.menus.support));
+                // {
+                //     "template": "tree",
+                //     "items": [
+                //         {
+                //             "template": "link",
+                //             "attributes": {
+                //                 "href": "[LINK DEFINED BY HELO LINKS INPUT]"
+                //             },
+                //             "text": "Help Link"
+                //         }
+                //     ]
+                // },
+                // {
+                //     "template": "tree",
+                //     "items": [
+                //         {
+                //             "template": "link",
+                //             "attributes": {
+                //                 "href": "[LINK DEFINED BY SUPPORT LINK INPUT]"
+                //             },
+                //             "text": "Support"
+                //         },
+                //         {
+                //             "template": "link",
+                //             "attributes": {
+                //                 "href": "[LINK DEFINED BY CONTACT US LINK INPUT]"
+                //             },
+                //             "text": "Contact Us"
+                //         }
+                //     ]
+                // }
+
+                var supportPopover = {
+                    "popover": {
+                        "contents": [
+                            {
+                                "template": "popover",
+                                "contents": [
+                                ]
+                            }
+                        ]
+                    }
+                };
+
+                // Add the account menu if it exists
+                if (fwData && fwData.menus && fwData.menus.addHelp) {
+                    supportPopover.popover.contents[0].contents.push(fwData.menus.addHelp);
+                }
+
+                // Add the account menu if it exists
+                if (fwData && fwData.menus && fwData.menus.supportContact) {
+                    supportPopover.popover.contents[0].contents.push(fwData.menus.supportContact);
+                }
+
+                dSupportButton.addEventListener('click', _events.getSupportPopover.bind(null, supportPopover));
 
                 journal.log({ type: 'info', owner: 'UI', module: 'emp', function: 'externalApp' }, 'Support Menu setup');
             }
