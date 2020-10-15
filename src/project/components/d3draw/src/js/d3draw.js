7e0318ef9bd69166f23cae17f4fc4c81058352d7
Jeff Herb <herb.jeff@gmail.com>
2020-10-15 13:16:29 -0400

    D3 now loads!

--- src/project/components/d3draw/src/js/d3draw.js
+++ src/project/components/d3draw/src/js/d3draw.js
@@ -1,4 +1,7 @@
-define([], function() {
+// The d3 library needs to be custom build from source in the AMD output format to get it to work!
+define(['d3'], function(d3) {
+
+    console.log(d3);
 
     console.log("D3 Draw Component called!");
 
